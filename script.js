49d2fcb86f24ed598e66433fb19c65ddc40ef4ff
josueaquino12 <aquinojosuebenjamin@gmail.com>
2024-07-23 15:49:51 -0300

    Agregamos el JS y la aplicacion de clima lista

---
+++ script.js
@@ -0,0 +1,62 @@
+let api_key = 'a1b7c156fe7f0544134121be9062dccb'
+
+let urlBase = 'https://api.openweathermap.org/data/2.5/weather'
+
+let difKelvin = 275.15
+
+let ciudad = 'Paris'
+
+
+
+
+  document.getElementById('botonBusqueda').addEventListener('click', () => {
+    const ciudad = document.getElementById('ciudadEntrada').value
+    if(ciudad){
+        fetchDatosCLima(ciudad)
+    }
+  })
+
+  function fetchDatosCLima(ciudad){
+
+  fetch(`${urlBase}?q=${ciudad}&appid=${api_key}`)
+  .then(data => data.json())
+  .then(data =>mostrarDatosClima(data))
+
+  }
+
+  function mostrarDatosClima(data){
+
+    const divDatosClima = document.getElementById('datosClima')
+    divDatosClima.innerHTML=''
+    //const ciudadNombre = data.ciudadNombre
+    const ciudadNombre = data.name
+    const paisNombre = data.sys.country
+    const temperatura = data.main.temp
+    const humedad = data.main.humidity
+    /*const descripcion = data.weather[0].description*/
+    const icono = data.weather[0].icon
+
+    const ciudadTitulo = document.createElement('h2')
+    ciudadTitulo.textContent = `${ciudadNombre}, ${paisNombre}`
+
+    const temperaturaInfo = document.createElement('p')
+    temperaturaInfo.textContent = `La temperatura es: ${Math.floor(temperatura-difKelvin)}ºC `
+
+    const humedadInfo = document.createElement('p')
+    humedadInfo.textContent = `La humedad es: ${humedad}% `
+
+    const iconoInfo = document.createElement('img')
+    iconoInfo.src = `https://openweathermap.org/img/wn/${icono}@2x.png`
+
+    /*const descripcionInfo = document.createElement('p')
+    descripcionInfo.textContent = `La descripción meteorológica es: ${descripcion}`*/
+
+    divDatosClima.appendChild(ciudadTitulo)
+    divDatosClima.appendChild(temperaturaInfo)
+    divDatosClima.appendChild(iconoInfo)
+    divDatosClima.appendChild(humedadInfo)
+    /*divDatosClima.appendChild(descripcionInfo)*/
+    
+
+  }
+
